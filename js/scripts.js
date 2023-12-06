fd4729bbdcd0e6eec668df8ffce9c5a84ed58663
Dr-Zancho <119409873+Dr-Zancho@users.noreply.github.com>
2023-12-06 21:43:04 +0100

    test

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - New Age v6.0.7 (https://startbootstrap.com/theme/new-age)
+* Start Bootstrap - Grayscale v7.0.6 (https://startbootstrap.com/theme/grayscale)
 * Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-new-age/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
 */
 //
 // Scripts
@@ -9,12 +9,32 @@
 
 window.addEventListener('DOMContentLoaded', event => {
 
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
     // Activate Bootstrap scrollspy on the main nav element
     const mainNav = document.body.querySelector('#mainNav');
     if (mainNav) {
         new bootstrap.ScrollSpy(document.body, {
             target: '#mainNav',
-            offset: 74,
+            rootMargin: '0px 0px -40%',
         });
     };
 
